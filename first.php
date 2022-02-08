f2bb3886a5e56b2b18c0a1b34af31e293379a6c0
Ezrah117 <99257485+Ezrah117@users.noreply.github.com>
2022-02-08 17:01:30 +0000

    Add files via upload

---
+++ first.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Basic PHP Page</title>
+</head>
+
+<body>
+    <p>This is standard HTML</p>
+    <?php
+    echo 'This was generated using php!';
+    ?> 
+    </body>
+</html>
