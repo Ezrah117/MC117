e2bbbf80ef937ae87fed34bbfcddff4a45fbf3ac
Wesley Rademan <wesley.rademan@student.griffith.ie>
2022-02-25 19:37:08 +0000

    Lab 3 Final

---
+++ Labs/Lab 3.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Lab 2</title>
+    </head>
+
+    <body>
+        <?php
+        //QUESTION 1
+        $word = array('fox' => 0, 'jump' => 1, 'quick' => 2, 'brown' => 3, 'lazy' => 4, 'dog' => 5);
+        echo "<br>";
+        echo nl2br("Question 1 \r\n");
+        echo "The \r\n";
+        echo nl2br("$word[2] \r\n");
+        echo nl2br("$word[3] \r\n");
+        echo nl2br("$word[0] \r\n");
+        echo nl2br("$word[1] \r\n"."s");
+        echo "over the";
+        echo nl2br("$word[4] \r\n");
+        echo nl2br("$word[5] \r\n");
+
+        ?>
