932646e02b8cec1b7a3ccee441554888d17d0e71
Wesley Rademan <wesley.rademan@student.griffith.ie>
2022-02-25 19:40:25 +0000

    Lab 3 Final

--- Labs/Lab 3.php
+++ Labs/Lab 3.php
@@ -8,16 +8,52 @@
     <body>
         <?php
         //QUESTION 1
-        $word = array('fox' => 0, 'jump' => 1, 'quick' => 2, 'brown' => 3, 'lazy' => 4, 'dog' => 5);
+        $word = array('fox', 'jump', 'quick', 'brown', 'lazy', 'dog');
         echo "<br>";
         echo nl2br("Question 1 \r\n");
-        echo "The \r\n";
-        echo nl2br("$word[2] \r\n");
-        echo nl2br("$word[3] \r\n");
-        echo nl2br("$word[0] \r\n");
-        echo nl2br("$word[1] \r\n"."s");
-        echo "over the";
-        echo nl2br("$word[4] \r\n");
-        echo nl2br("$word[5] \r\n");
+        echo "The ".$word[2]. " ".$word[3]." ".$word[0]." ".$word[1]."s "."over the ".$word[4]." ".$word[5]; 
+        echo "<br>";
 
+        //QUESTION 2
+        $arr = range(10, 50, 10);
+        echo "<br>";
+        echo nl2br("Quesion 2 \r\n");
+        print_r($arr);
+        echo"<br>";
+        unset($arr[3]);
+        $arr = array_values($arr);
+        print_r($arr);
+        echo "<br>";
+
+        //QUESTION 3
+        $arr = array("Sophia" => "10", "Jacob" => "21", "William" => "29", "Alex" => "43");
+        echo "<br>";
+        echo nl2br("Question 3 \r\n");
+        asort($arr);
+        print_r($arr);
+        echo "<br>";
+        ksort($arr);
+        print_r($arr);
+        echo "<br>";
+        arsort($arr);
+        print_r($arr);
+        echo "<br>";
+        krsort($arr);
+        print_r($arr);
+        echo "<br>";
+
+        //Question 4
+        $color = array(
+        "color" => array ("a" => "Red", "b" => "Green", "c" => "White"),
+        "numbers" => array(1, 2, 3, 4, 5, 6),
+        "holes" => array("First", 5=> "Second", "Third"));
+        echo "<br>";
+        echo nl2br("Question 4 \r\n");
+        echo $color["color"]["c"];
+        echo"<br>";
+        echo $color["holes"][0];
+        echo "<br>";
+        echo $color["numbers"][4];
         ?>
+    </body>
+</html>
